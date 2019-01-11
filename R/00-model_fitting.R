432f9fe6a348f400865b3155e34fe43ca281d1bb
Rani <rani.schwindt@gmail.com>
2019-01-10 21:18:38 -0700

    Update README and model fitting

--- R/00-model_fitting.R
+++ R/00-model_fitting.R
@@ -1,26 +1,30 @@
 # -----------------------------------------------------------------------------
 # Script 00: Human Plasma Data Cleaning & Linear Model Fitting
 # Author: Rani Powers
-# Last updated: July 31, 2018
+# Last updated: December 11, 2018
 #
-# Reads in the human metabolomics eset, filters low abundance metabolites and 
-# metabolites in < 90% of the T21 and/or D21 samples. 
+# Reads in Data/human_plasma_metabolomics_eset.rds, filters low abundance 
+# metabolites and metabolites in < 90% of the T21 and/or D21 samples. 
 #
-# Identifies and filters two outlier samples.
+# Outputs:
+# 1) Saves Data/human_plasma_metabolomics_eset_filtered.rds - eset with the raw
+# data after the filtering steps described above.
 #
-# Saves density and PCA plots from pre- and post-adjustment (batch, age, sex)
-# in the Results/Figure_1/ folder.
+# 2) Saves density plots from pre- and post-adjustment (batch, age, sex) in the
+# Results/Supplementary_Figures/ folder.
 # 
-# Fits linear model to data with batch, age and sex as covariates. Saves the
-# model residuals in a new eset (Human_plasma_residuals.rds).
+# 3) Fits linear model to data with batch, age and gender as covariates. Saves 
+# the model residuals in a new eset (Data/human_plasma_residuals.rds).
 # -----------------------------------------------------------------------------
 
 # Load libraries, color palettes and plotting functions
 source('R/helpers.R')
-if (!dir.exists('Results/Figure_1')) { dir.create('Results/Figure_1', recursive = T) }
+if (!dir.exists('Results')){
+  dir.create('Results/Supplementary_Figures/', recursive = T)
+}
 
 # Read in human metabolomics data from Nexus and HTP cohorts
-eset = readRDS('Data/Human_plasma_metabolomics.rds')
+eset = readRDS('Data/human_plasma_metabolomics_eset.rds')
 met_data = as.data.frame(exprs(eset))
 sample_data = pData(eset)
 feature_data = fData(eset)
@@ -41,9 +45,8 @@ met_data = met_data[to_keep,]
 met_data_log2 = log2(met_data[to_keep,])
 
 # Plot density plot using the log2 data (no model fitting)
-outliers = c('222762382', '222763227')  # We know these are > 2.5 sd from mean - see line 116
 d = list()
-pdf('Results/Figure_1/Fig1_Supp2A_withOutliers.pdf',
+pdf('Results/Supplementary_Figures/Supp_Fig_1b_density_plot_before_fitting.pdf',
     height = 5, width = 5)
 plot(density(na.omit(as.numeric(met_data_log2[,1]))), 
      col = ifelse(sample_data$Batch[1] == 1, cols[11], cols[8]),
@@ -51,109 +54,22 @@ plot(density(na.omit(as.numeric(met_data_log2[,1]))),
      ylim = c(0, .2), las = 1, xlab = 'log2 intensity (91 metabolites)')
 for (i in 2:ncol(met_data_log2)){
   lines(density(na.omit(as.numeric(met_data_log2[,i]))),
-        col = ifelse(sample_data$Barcode[i] %in% outliers, 'red',
-                     ifelse(sample_data$Batch[i] == 1, cols[11], cols[8])))
+        col = ifelse(sample_data$Batch[i] == 1, cols[11], cols[8]))
   d = c(d, list(density(na.omit(as.numeric(met_data_log2[,i])))))
 }
 legend('topright', 
        lty = 1, col = c(cols[11], cols[8]),
-       legend = c('Nexus', 'HTP'))
-dev.off()
-
-# Plot PCA using the log2 data (no model fitting)
-met_data_log2_NAexcluded = na.omit(met_data_log2)
-log2_pca = summary(prcomp(t(met_data_log2_NAexcluded), center = T, scale. = T))
-pdf('Results/Figure_1/Fig1_Supp2C_withOutliers.pdf',
-    height = 5, width = 5)
-plot_batch_pca(log2_pca, bad = outliers,
-               main = 'PCA of log2 data\nno model fitting',
-               legend_side = 'topleft')
+       legend = c('Cohort 1', 'Cohort 2'))
 dev.off()
 
 # Fit linear model with batch, age and sex as covariates
-adjusted_met_data = removeBatchEffect(met_data_log2, 
-                            batch = sample_data$Batch, 
-                            design = model.matrix(~0+Age+Sex, data = sample_data))
-
-# Plot density plot using adjusted data (after model fitting)
-pdf('Results/Figure_1/Fig1_Supp2B_withOutliers.pdf',
-    height = 5, width = 5)
-plot(density(adjusted_met_data[,1]), 
-     col = ifelse(sample_data$Batch[1] == 1, cols[11], cols[8]),
-     main = 'Density of log2 data after model fitting\n(covariates = batch, age, sex)',
-     ylim = c(0, .2), las = 1)
-for (i in sample(2:ncol(adjusted_met_data))){
-  lines(density(na.omit(adjusted_met_data[,i])),
-        col = ifelse(sample_data$Barcode[i] %in% outliers, 'red',
-                     ifelse(sample_data$Batch[i] == 1, cols[11], cols[8])))
-}
-legend('topleft', 
-       lty = 1, col = c(cols[11], cols[8]),
-       legend = c('Nexus', 'HTP'))
-dev.off()
-
-# Plot PCA using adjusted data (after model fitting)
-adjusted_NAexcluded = na.omit(adjusted_met_data)
-adjusted_pca = summary(prcomp(t(adjusted_NAexcluded), center = T, scale. = T))
-# Draw lines showing > 3 SD from the mean on PCA
-pca_data = as.data.frame(adjusted_pca$x)
-pc1 = pca_data$PC1
-pc2 = pca_data$PC2
-pdf('Results/Figure_1/Fig1_Supp2D_withOutliers.pdf',
-    height = 5, width = 5)
-plot_batch_pca(adjusted_pca, bad = outliers,
-               main = 'PCA of log2 data after model fitting\n(covariates = batch, age, sex)',
-               legend_side = 'bottomright')
-abline(v = sd(pc1)*3, col = 'grey')
-abline(v = sd(pc1)*-3, col = 'grey')
-abline(h = sd(pc2)*3, col = 'grey')
-abline(h = sd(pc2)*-3, col = 'grey')
-dev.off()
-
-# Remove outliers
-sd_cutoff = 2.5
-outside_of_3sd_pc1 = row.names(pca_data[pc1 > sd(pc1) * sd_cutoff | pc1 < sd(pc1) * -1*sd_cutoff,])
-outside_of_3sd_pc2 = row.names(pca_data[pc2 > sd(pc2) * sd_cutoff | pc2 < sd(pc2) * -1*sd_cutoff,])
-outliers = intersect(outside_of_3sd_pc1, outside_of_3sd_pc2)
-met_data_log2 = met_data_log2[,!names(met_data_log2) %in% outliers]
-sample_data = sample_data[!row.names(sample_data) %in% outliers,]
-
-# Plot density plot using the log2 data (no model fitting)
-d = list()
-pdf('Results/Figure_1/Fig1_Supp2A.pdf',
-    height = 5, width = 5)
-plot(density(na.omit(as.numeric(met_data_log2[,1]))), 
-     col = ifelse(sample_data$Batch[1] == 1, cols[11], cols[8]),
-     main = 'Density of log2 data\nno model fitting',
-     ylim = c(0, .2), las = 1, xlab = 'log2 intensity (91 metabolites)')
-for (i in 2:ncol(met_data_log2)){
-  lines(density(na.omit(as.numeric(met_data_log2[,i]))),
-        col = ifelse(sample_data$Barcode[i] %in% outliers, 'red',
-                     ifelse(sample_data$Batch[i] == 1, cols[11], cols[8])))
-  d = c(d, list(density(na.omit(as.numeric(met_data_log2[,i])))))
-}
-legend('topright', 
-       lty = 1, col = c(cols[11], cols[8]),
-       legend = c('Nexus', 'HTP'))
-dev.off()
-
-# Plot PCA using the log2 data (no model fitting)
-met_data_log2_NAexcluded = na.omit(met_data_log2)
-log2_pca = summary(prcomp(t(met_data_log2_NAexcluded), center = T, scale. = T))
-pdf('Results/Figure_1/Fig1_Supp2C.pdf',
-    height = 5, width = 5)
-plot_batch_pca(log2_pca, bad = outliers,
-               main = 'PCA of log2 data\nno model fitting',
-               legend_side = 'topleft')
-dev.off()
-
-# Fit linear model with batch, age and sex as covariates
+# (Use removeBatchEffect to get adjusted intensity values for plotting)
 adjusted_met_data = removeBatchEffect(met_data_log2, 
                                       batch = sample_data$Batch, 
                                       design = model.matrix(~0+Age+Sex, data = sample_data))
 
 # Plot density plot using adjusted data (after model fitting)
-pdf('Results/Figure_1/Fig1_Supp2B.pdf',
+pdf('Results/Supplementary_Figures/Supp_Fig_1c_density_plot_after_fitting.pdf',
     height = 5, width = 5)
 plot(density(adjusted_met_data[,1]), 
      col = ifelse(sample_data$Batch[1] == 1, cols[11], cols[8]),
@@ -161,22 +77,11 @@ plot(density(adjusted_met_data[,1]),
      ylim = c(0, .2), las = 1)
 for (i in sample(2:ncol(adjusted_met_data))){
   lines(density(na.omit(adjusted_met_data[,i])),
-        col = ifelse(sample_data$Barcode[i] %in% outliers, 'red',
-                     ifelse(sample_data$Batch[i] == 1, cols[11], cols[8])))
+        col = ifelse(sample_data$Batch[i] == 1, cols[11], cols[8]))
 }
 legend('topleft', 
        lty = 1, col = c(cols[11], cols[8]),
-       legend = c('Nexus', 'HTP'))
-dev.off()
-
-# Plot PCA using adjusted data (after model fitting)
-adjusted_NAexcluded = na.omit(adjusted_met_data)
-adjusted_pca = summary(prcomp(t(adjusted_NAexcluded), center = T, scale. = T))
-pdf('Results/Figure_1/Fig1_Supp2D.pdf',
-    height = 5, width = 5)
-plot_batch_pca(adjusted_pca, bad = outliers,
-               main = 'PCA of log2 data after model fitting\n(covariates = batch, age, sex)',
-               legend_side = 'topleft')
+       legend = c('Cohort 1', 'Cohort 2'))
 dev.off()
 
 # Fit linear model and save residuals
@@ -184,7 +89,7 @@ design_covars = model.matrix(~0+as.factor(Batch)+Age+as.factor(Sex),
                              sample_data[,c('Batch', 'Age', 'Sex')])
 fit = lmFit(met_data_log2, design = design_covars)
 model_residuals = residuals(fit, met_data_log2)
-saveRDS(model_residuals, 'Data/Human_plasma_residuals.rds')
+saveRDS(model_residuals, 'Data/human_plasma_residuals.rds')
 
 # Save final, filtered metabolomics data
 EXPRS_DATA = met_data_log2
@@ -192,7 +97,7 @@ PHENO_DATA = AnnotatedDataFrame(data = sample_data[names(EXPRS_DATA),],
                                 dimLabels = c('Barcode', 'Info'))
 FEATURE_DATA = AnnotatedDataFrame(feature_data[row.names(EXPRS_DATA),])
 eset = new("ExpressionSet", 
-            exprs = EXPRS_DATA, 
-            phenoData = PHENO_DATA, 
-            featureData = FEATURE_DATA)
-saveRDS(eset, 'Data/Human_plasma_metabolomics_filtered.rds')
+           exprs = EXPRS_DATA, 
+           phenoData = PHENO_DATA, 
+           featureData = FEATURE_DATA)
+saveRDS(eset, 'Data/human_plasma_metabolomics_eset_cleaned.rds')
--- README.md
+++ README.md
@@ -1,10 +1,15 @@
-## Trisomy 21 drives production of neurotoxic tryptophan catabolites via the interferon-inducible kynurenine pathway
+---
+output:
+  html_document: default
+  pdf_document: default
+---
+## Trisomy 21 activates the kynurenine pathway via increased dosage of interferon receptors
 
 Our preprint is available on bioRxiv [here](https://www.biorxiv.org/content/early/2018/08/29/403642).
 
 **Authors:**
 
-Rani K. Powers<sup>1,2,3#</sup>, Kelly D. Sullivan<sup>1,3#</sup>, Rachel Culp-Hill<sup>4</sup>, Michael P. Ludwig<sup>1</sup>, Keith P. Smith<sup>1</sup>, Katherine A. Waugh<sup>1</sup>, Ross Minter<sup>1</sup>, Kathryn D. Tuttle<sup>1</sup>, Angela L. Rachubinski<sup>1</sup>, Ross E. Granrath<sup>1</sup>, Rebecca B. Wilkerson<sup>4</sup>, Angelo D’Alessandro<sup>1,4</sup>, James C. Costello<sup>2,3,&ast;</sup> & Joaquin M. Espinosa<sup>1,3,5&ast;</sup>
+Rani K. Powers<sup>1,2,3#</sup>, Kelly D. Sullivan<sup>1,3,4,5#</sup>, Rachel Culp-Hill<sup>6</sup>, Michael P. Ludwig<sup>1,3</sup>, Keith P. Smith<sup>1</sup>, Katherine A. Waugh<sup>1</sup>, Ross Minter<sup>1</sup>, Kathryn D. Tuttle<sup>1</sup>, Hannah C. Lewis<sup>1</sup>, Angela L. Rachubinski<sup>1,5</sup>, Ross E. Granrath<sup>1</sup>, Rebecca B. Wilkerson<sup>6</sup>, Darcy E. Kahn<sup>1</sup>, Molishree Joshi<sup>4</sup>, Angelo D’Alessandro<sup>1,6</sup>, James C. Costello<sup>2,3,&ast;</sup> & Joaquin M. Espinosa<sup>1,3,4,7&ast;</sup>
 
 **Affiliations:**
 
@@ -14,9 +19,14 @@ Rani K. Powers<sup>1,2,3#</sup>, Kelly D. Sullivan<sup>1,3#</sup>, Rachel Culp-H
 
 <sup>3</sup> Department of Pharmacology, University of Colorado Anschutz Medical Campus, Aurora, CO, USA 
 
-<sup>4</sup> Department of Biochemistry and Molecular Genetics, University of Colorado Anschutz Medical Campus,
+<sup>4</sup> Functional Genomics Facility, University of Colorado Anschutz Medical Campus, Aurora, CO, USA
+
+<sup>5</sup> Department of Pediatrics, University of Colorado Anschutz Medical Campus, Aurora, CO, USA
+
+<sup>6</sup> Department of Biochemistry and Molecular Genetics, University of Colorado Anschutz Medical Campus,
 Aurora, CO, USA
-<sup>5</sup> Department of Molecular, Cellular and Developmental Biology, University of Colorado Boulder, Boulder, CO, USA
+
+<sup>7</sup> Department of Molecular, Cellular and Developmental Biology, University of Colorado Boulder, Boulder, CO, USA
 
 <sup>#</sup> These authors contributed equally to this work
 
@@ -28,21 +38,22 @@ Joaquin M. Espinosa: joaquin.espinosa@ucdenver.edu
 
 ---
 
-### Included in this Repo
+### Included in this repository
 
-To ensure reproducibility, all data for this project can be found in the Data/ directory and the R code to generate all figures can be found in the R/ directory. Descriptions for each data file and R script are below.
+To ensure reproducibility, all data for this project can be found in the Data directory and the R code to generate all figures can be found in the R directory.
 
 #### Data
 
-* **Human_plasma_metabolomics.csv** - Cohort 1 and Cohort 2 participant characteristics and raw intensity values (relative abundance) from semi-targeted LC-MS plasma metabolomics
-* **Human_plasma_metabolomics.csv** - Same data as Human_plasma_metabolomics.csv formatted as an R [eset](https://www.bioconductor.org/packages/3.7/bioc/vignettes/Biobase/inst/doc/ExpressionSetIntroduction.pdf) object
-* **Human_plasma_MSD.csv** - Mesoscale discovery assay data from a subset of Cohort 2 samples
-* **Human_fibroblast_flux.csv** - Targeted LC-MS flux metabolomics data for timecourse experiment on human fibroblasts
-* **Mouse_plasma_metabolomics.csv** - Targeted LC-MS metabolomics data on plasma from D21 and T21 (Dp10, Dp16 and Dp17) mice
+* **human_plasma_metabolomics.csv** - Cohort 1 and Cohort 2 participant characteristics and raw, LC-MS plasma metabolomics intensity values (relative abundance)
+* **human_plasma_metabolomics_eset.rds** - Same data as Human_plasma_metabolomics.csv formatted as an R [eset](https://www.bioconductor.org/packages/3.7/bioc/vignettes/Biobase/inst/doc/ExpressionSetIntroduction.pdf) object
+* **human_plasma_MSD.csv** - Mesoscale discovery assay data from Cohort 2 human plasma samples
+* **human_fibroblast_flux.csv** - Targeted metabolic flux data from timecourse experiment on human fibroblasts with IFN-α stimulation
+* **human_fibroblast_flux_KO.csv** - Targeted metabolic flux data from timecourse experiment on human fibroblasts with IFNAR knockout and IFN-α stimulation
+* **Mouse_plasma_metabolomics.csv** - Targeted metabolomics data on plasma from WT and T21 (Dp10, Dp16 and Dp17 strains) mice
 
 #### Code
 
-To reproduce the figures from this project, first make sure you have all dependencies installed the run each of the numbered R scripts in order. These scripts will create a folder called Results/ and each script will subsequently output plots and/or tables to an appropriate subfolder named for each figure (e.g. Result/Figure_1/). General descriptions of each script are below, and additional information can be found in the headers of the scripts themselves.
+To reproduce the figures from our paper, make sure you have all dependencies installed then run each of the numbered R scripts in order. These scripts will create a directory called Results and output plots and/or tables to an appropriate subdirectory named for each figure (e.g. Results/Figure_1). General descriptions of each script are below, and additional information can be found at the beginning of the scripts themselves.
 
 Dependencies:
 
@@ -54,12 +65,12 @@ Dependencies:
 
 R Scripts:
 
-* **00-model_fitting.R** - pre-processes human metabolomics data and fits linear model with batch, age and sex covariates. Saves plots for Figure 1 figure supplement 2A-D in the Results/Figure_1/ folder.
-* **01-differential_abundance.R** - performs differential abundance analysis on human plasma samples. Saves plots for Figure 1A and Figure 1 figure supplement 1B-C in the Results/Figure_1/ folder.
-* **02-pathway_enrichment.R** - performs hypergeometric testing for pathway enrichment on human plasma samples. Saves plot for Figure 1C and supplemental table of results in the Results/Figure_1/ folder.
-* **03-boxplots_t21_vs_d21** - plots boxplots of differentially abundant metabolites, tryptophan pathway metabolites and metabolite ratios. Saves plots for Figure 1B, Figure 3, and Figure 3 supplement 1 in the Results/Figure_1 and Results/Figure_3 folders.
-* **04-lasso_biomarker.R** - performs univariate and multivariate biomarker analysis to learn a metabolic signature of trisomy 21. Saves plots for Figure 2A-C in the Results/Figure_2 folder.
-* **05-fibroblast_timecourse.R** - pre-processes and plots timecourse data from the flux metabolomics experiment on human fibroblasts. Saves plots for Figure 4A-C and Figure 4 figure supplement 1A-D in the Results/Figure_4 folder.
-* **06-cytokine_correlations.R** - correlates kynurenine and quinolinic acid in human plasma with Mesoscale Discovery assay data. Saves plots for Figure 6 in the Results/Figure_6 folder.
-* **07-mouse_kynurenine.R** - plots kynurenine levels in mouse plasma with and without treatment with poly(I:C). Saves plots for Figure 5 in the Results/Figure_5 folder.
+* **00-model_fitting.R** - pre-processes human metabolomics data and fits linear model with batch, age and sex covariates. Saves density plots for Supplementary Figure 1b-c in the Results/Supplementary_Figures directory.
+* **01-differential_abundance.R** - performs differential abundance analysis on human plasma samples. Saves plots for Figure 1A and Figure 1 figure supplement 1B-C in the Results/Figure_1 directory.
+* **02-pathway_enrichment.R** - performs hypergeometric testing for pathway enrichment on human plasma samples. Saves plot for Figure 1C and supplemental table of results in the Results/Figure_1 directory.
+* **03-boxplots_t21_vs_d21** - plots boxplots of differentially abundant metabolites, tryptophan pathway metabolites and metabolite ratios. Saves plots for Figure 1B, Figure 3, and Figure 3 supplement 1 in the Results/Figure_1 and Results/Figure_3 directories.
+* **04-lasso_biomarker.R** - performs univariate and multivariate biomarker analysis to learn a metabolic signature of trisomy 21. Saves plots for Figure 2A-C in the Results/Figure_2 directory.
+* **05-fibroblast_timecourse.R** - pre-processes and plots timecourse data from the flux metabolomics experiment on human fibroblasts. Saves plots for Figure 4A-C and Figure 4 figure supplement 1A-D in the Results/Figure_4 directory.
+* **06-cytokine_correlations.R** - correlates kynurenine and quinolinic acid in human plasma with Mesoscale Discovery assay data. Saves plots for Figure 6 in the Results/Figure_6 directory.
+* **07-mouse_kynurenine.R** - plots kynurenine levels in mouse plasma with and without treatment with poly(I:C). Saves plots for Figure 5 in the Results/Figure_5 directory.
 
