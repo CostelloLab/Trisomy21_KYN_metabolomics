8ba6cd15f88a0aa0e604b7a8bc3c5186169bd6c1
Rani <rani.schwindt@gmail.com>
2019-08-15 15:30:28 -0600

    Clean up cohort 1 and 2 metabolomics preprocessing

--- R/00-model_fitting.R
+++ R/00-model_fitting.R
@@ -1,30 +1,27 @@
 # -----------------------------------------------------------------------------
 # Script 00: Human Plasma Data Cleaning & Linear Model Fitting
 # Author: Rani Powers
-# Last updated: December 11, 2018
 #
-# Reads in Data/human_plasma_metabolomics_eset.rds, filters low abundance 
-# metabolites and metabolites in < 90% of the T21 and/or D21 samples. 
+# Reads in the human metabolomics eset, filters low abundance metabolites and 
+# metabolites in < 90% of the T21 and/or D21 samples. Performs age-, sex- and
+# batch-adjustment using linear model.
 #
 # Outputs:
-# 1) Saves Data/human_plasma_metabolomics_eset_filtered.rds - eset with the raw
+# 1) Saves supplementary density plots from pre- and post-adjustment (batch, age, sex)
+# in the Results/Supplementary_Figures/ folder.
+# 
+# 2) Saves Data/Human_plasma_metabolomics_eset_filtered.rds - eset with the raw
 # data after the filtering steps described above.
 #
-# 2) Saves density plots from pre- and post-adjustment (batch, age, sex) in the
-# Results/Supplementary_Figures/ folder.
-# 
-# 3) Fits linear model to data with batch, age and gender as covariates. Saves 
-# the model residuals in a new eset (Data/human_plasma_residuals.rds).
+# 3) Saves Data/Human_plasma_metabolomics_eset_filtered_adjusted.rds - eset with 
+# the adjusted data after age/sex/batch adjustment.
 # -----------------------------------------------------------------------------
 
 # Load libraries, color palettes and plotting functions
 source('R/helpers.R')
-if (!dir.exists('Results')){
-  dir.create('Results/Supplementary_Figures/', recursive = T)
-}
 
 # Read in human metabolomics data from Nexus and HTP cohorts
-eset = readRDS('Data/human_plasma_metabolomics_eset.rds')
+eset = readRDS('Data/Human_plasma_metabolomics_eset.rds')
 met_data = as.data.frame(exprs(eset))
 sample_data = pData(eset)
 feature_data = fData(eset)
@@ -36,17 +33,18 @@ met_d21 = sample_data[sample_data$Karyotype == 'D21', 'Barcode']
 in_t21 = apply(met_data[,met_t21], 1, function(x) sum(!is.na(x))) >= length(met_t21) * .9
 in_d21 = apply(met_data[,met_d21], 1, function(x) sum(!is.na(x))) >= length(met_d21) * .9
 to_keep = in_t21 | in_d21
-feature_data[names(to_keep[to_keep == F]), 'Compound_Name']
 to_keep = names(to_keep[to_keep == T])
 met_data = met_data[to_keep,]
+feature_data = feature_data[to_keep,]
 # 91 metabolites meet the criteria
 
 # Use log2 data
 met_data_log2 = log2(met_data[to_keep,])
 
-# Plot density plot using the log2 data (no model fitting)
+## PLOT 
+# Density plot using the log2 data (no model fitting)
 d = list()
-pdf('Results/Supplementary_Figures/Supp_Fig_1b_density_plot_before_fitting.pdf',
+pdf('Results/Supplementary_Figures/SuppFig_1B_densityPlot_beforeFitting.pdf',
     height = 5, width = 5)
 plot(density(na.omit(as.numeric(met_data_log2[,1]))), 
      col = ifelse(sample_data$Batch[1] == 1, cols[11], cols[8]),
@@ -68,8 +66,9 @@ adjusted_met_data = removeBatchEffect(met_data_log2,
                                       batch = sample_data$Batch, 
                                       design = model.matrix(~0+Age+Sex, data = sample_data))
 
-# Plot density plot using adjusted data (after model fitting)
-pdf('Results/Supplementary_Figures/Supp_Fig_1c_density_plot_after_fitting.pdf',
+## PLOT
+# Density plot using adjusted data (after model fitting)
+pdf('Results/Supplementary_Figures/SuppFig_1C_densityPlot_afterFitting.pdf',
     height = 5, width = 5)
 plot(density(adjusted_met_data[,1]), 
      col = ifelse(sample_data$Batch[1] == 1, cols[11], cols[8]),
@@ -84,13 +83,6 @@ legend('topleft',
        legend = c('Cohort 1', 'Cohort 2'))
 dev.off()
 
-# Fit linear model and save residuals
-design_covars = model.matrix(~0+as.factor(Batch)+Age+as.factor(Sex), 
-                             sample_data[,c('Batch', 'Age', 'Sex')])
-fit = lmFit(met_data_log2, design = design_covars)
-model_residuals = residuals(fit, met_data_log2)
-saveRDS(model_residuals, 'Data/human_plasma_residuals.rds')
-
 # Save final, filtered metabolomics data
 EXPRS_DATA = met_data_log2
 PHENO_DATA = AnnotatedDataFrame(data = sample_data[names(EXPRS_DATA),],
@@ -100,4 +92,15 @@ eset = new("ExpressionSet",
            exprs = EXPRS_DATA, 
            phenoData = PHENO_DATA, 
            featureData = FEATURE_DATA)
-saveRDS(eset, 'Data/human_plasma_metabolomics_eset_cleaned.rds')
+saveRDS(eset, 'Data/Human_plasma_metabolomics_eset_filtered.rds')
+
+# Save final, filtered metabolomics data with age/batch/sex adj data
+EXPRS_DATA = as.data.frame(adjusted_met_data)
+PHENO_DATA = AnnotatedDataFrame(data = sample_data[names(EXPRS_DATA),],
+                                dimLabels = c('Barcode', 'Info'))
+FEATURE_DATA = AnnotatedDataFrame(feature_data[row.names(EXPRS_DATA),])
+eset = new("ExpressionSet", 
+           exprs = EXPRS_DATA, 
+           phenoData = PHENO_DATA, 
+           featureData = FEATURE_DATA)
+saveRDS(eset, 'Data/Human_plasma_metabolomics_eset_filtered_adjusted.rds')
